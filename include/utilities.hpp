3c1e47cc99ae1f4501bc55c87508072ac985344c
Krakra123 <nghiabn2005@gmail.com>
2025-03-23 11:36:43 +0700

    minor fixes on utilities

--- include/utilities.hpp
+++ include/utilities.hpp
@@ -10,19 +10,19 @@
 #include <type_traits>
 
 template <typename T>
-concept Arithmetic = std::is_arithmetic_v<T>;
+concept MIA_Arithmetic = std::is_arithmetic_v<T>;
 template <typename T>
-concept Integral = std::is_integral_v<T>;
+concept MIA_Integral = std::is_integral_v<T>;
 template <typename T>
-concept SignedIntegral = Integral<T> && std::is_signed<T>::value;
+concept MIA_SignedIntegral = MIA_Integral<T> && std::is_signed<T>::value;
 template <typename T>
-concept UnsignedIntegral = Integral<T> && !SignedIntegral<T>;
+concept MIA_UnsignedIntegral = MIA_Integral<T> && !MIA_SignedIntegral<T>;
 template <typename T>
-concept FloatingPoint = std::is_floating_point_v<T>;
+concept MIA_FloatingPoint = std::is_floating_point_v<T>;
 template <typename T>
-concept SignedFloatingPoint = FloatingPoint<T> && std::is_signed<T>::value;
+concept MIA_SignedFloatingPoint = MIA_FloatingPoint<T> && std::is_signed<T>::value;
 template <typename T>
-concept UnsignedFloatingPoint = FloatingPoint<T> && !SignedFloatingPoint<T>;
+concept MIA_UnsignedFloatingPoint = MIA_FloatingPoint<T> && !MIA_SignedFloatingPoint<T>;
 
 #define MIA_STRING_EXPAND(X) #X
 #define MIA_STRING(X) MIA_STRING_EXPAND(X)
@@ -104,23 +104,15 @@ constexpr T clamp(const T& x, const T& lower, const T& upper) {
 }
 
 // Lerp
-template <typename T, Arithmetic T2>
+template <typename T, MIA_Arithmetic T2 = T>
 constexpr T lerp(const T& range_start, const T& range_end, const T2& percent) {
     const T2 one_minus_percent = static_cast<T2>(1.0) - percent;
     return range_start * one_minus_percent + range_end * percent;
 }
-template <Arithmetic T>
-constexpr T lerp(const T& range_start, const T& range_end, const T& percent) {
-    return lerp<T, T>(range_start, range_end, percent);
-}
 
 // In Range
-template <typename T, typename T2>
+template <typename T, typename T2 = T>
 constexpr bool in_range(const T& val, const T2& range_start, const T2& range_end) {
-    return val >= range_start && val < range_end;
-}
-template <typename T>
-constexpr bool in_range(const T& val, const T& range_start, const T& range_end) {
     return val >= range_start && val < range_end;
 }
 
